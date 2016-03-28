a8dacf7649c18fa993f49f21d302ee6731d79bbd
huntzhan <programmer.zhx@gmail.com>
2016-03-28 21:37:31 +0800

    Init.

---
+++ secret_code/metadata.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from __future__ import (division, absolute_import, print_function,
+                        unicode_literals)
+
+
+NAME = 'secret_code'
+VERSION = '0.1.0'
+AUTHORS = [
+    'huntzhan',
+]
+EMAILS = [
+    'programmer.zhx@gmail.com',
+]
+LICENSE = ''
+URL = ''
+DESCRIPTION = 'A Simple Tool To Standardize Filename Of Porn Video.'
